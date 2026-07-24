1325ead6de0a6e0d53b80b516d1b10f046cf889b
eyob42 <eyob42@users.noreply.github.com>
2026-07-24 22:42:41 +0300

    Add auth module with JWT strategy, guard, login and me endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,9 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({ 
+      isGlobal: true,
+    }),
     PrismaModule,
     AuthModule,
   ],
